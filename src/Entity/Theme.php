e78517e278c538ce0b54e06cbc04a2646dfba379
foad <foad.sdt@gmail.com>
2025-02-10 10:52:53 -0500

    Updated database name in .env and docker-compose.yaml, added 'Theme' entity and migration
    - Changed database name in .env and docker-compose.yaml files.
    - Added 'Theme' entity to the project.
    - Created migration for adding the 'Theme' table.

---
+++ src/Entity/Theme.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: "App\Repository\ThemeRepository")]
+#[ApiResource]
+class Theme
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $id = null;
+
+    #[ORM\Column(type: "string", length: 255)]
+    #[Assert\NotBlank]
+    private string $name;
+
+    #[ORM\Column(type: "json", nullable: true)]
+    private ?array $colors = null;
+
+    #[ORM\Column(type: "boolean")]
+    private bool $isDefault = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getColors(): ?array
+    {
+        return $this->colors;
+    }
+
+    public function setColors(?array $colors): self
+    {
+        $this->colors = $colors;
+
+        return $this;
+    }
+
+    public function isIsDefault(): bool
+    {
+        return $this->isDefault;
+    }
+
+    public function setIsDefault(bool $isDefault): self
+    {
+        $this->isDefault = $isDefault;
+
+        return $this;
+    }
+}
+
